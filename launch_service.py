22fbe8145c29f6943c8658b01b642bac9cf8274e
jgawrilo <justin.gawrilow@gmail.com>
2017-02-06 17:42:04 -0500

    R calls version

--- launch_service.py
+++ launch_service.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+from rpy2.robjects import r
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.packages import importr
+import rpy2.robjects as robjects
+
 import argparse
 import logging
 from flask import Flask, make_response, jsonify
@@ -19,6 +24,14 @@ import os
 
 app = Flask(__name__)
 api = Api(app)
+
+pandas2ri.activate()
+
+ci = importr('CausalImpact')
+base = importr('base')
+devtools = importr('devtools')
+dollar = base.__dict__['$']
+at = base.__dict__['@']
 
 logging.basicConfig(format='%(levelname)s %(asctime)s %(filename)s %(lineno)d: %(message)s')
 logger = logging.getLogger(__name__)
@@ -69,29 +82,75 @@ class ImpactPredictorAPI(Resource):
         args = self.reqparse.parse_args()
 
         treatment_control_data = pd.DataFrame.from_dict(args["data"]["series"])
-        treatment_control_data["string_date"] = treatment_control_data.apply(
-            lambda x: datetime.datetime.fromtimestamp(x['date']).isoformat().split("T")[0], axis=1)
-        treatment_control_data = treatment_control_data.set_index("string_date")
 
-        tmp_filename = "./tmp/" + datetime.datetime.now().isoformat().replace(":","_") + ".txt"
+        time_df = treatment_control_data[["date"]]
+
+        time_df.index = range(1,len(time_df)+1)
+
+        treatment_control_data = treatment_control_data[["target","control"]]
+
+        treatment_control_data.index = range(1,len(treatment_control_data)+1)
+
+        pre_start = time_df[time_df.date == args["data"]["pre_start"]].index[0]
+        pre_end = time_df[time_df.date == args["data"]["pre_end"]].index[0]
+        post_start = time_df[time_df.date == args["data"]["post_start"]].index[0]
+        post_end = time_df[time_df.date == args["data"]["post_end"]].index[0]
+
+        columns = [
+            "response",
+            "cum.response",
+            "point.pred",
+            "point.pred.lower",
+            "point.pred.upper",
+            "cum.pred",
+            "cum.pred.lower",
+            "cum.pred.upper",
+            "point.effect",
+            "point.effect.lower",
+            "point.effect.upper",
+            "cum.effect",
+            "cum.effect.lower",
+            "cum.effect.upper"
+            ]
+
+        r_data = pandas2ri.py2ri(treatment_control_data)
+        pre = robjects.IntVector((pre_start,pre_end))
+        post = robjects.IntVector((post_start,post_end))
+        results = ci.CausalImpact(r_data,pre,post)
+        py_out = pandas2ri.ri2py(results)
+
+        dout = pandas2ri.ri2py_dataframe(dollar(py_out,"series"))
+
+        dout.columns = columns
+
+        dout.index = range(1,len(dout)+1)
+
+        final = pd.concat([time_df,dout],axis=1)
+
+        impact = dollar(dollar(py_out,"summary"),"AbsEffect")[1]
+        #treatment_control_data["string_date"] = treatment_control_data.apply(
+        #    lambda x: datetime.datetime.fromtimestamp(x['date']).isoformat().split("T")[0], axis=1)
+        #treatment_control_data = treatment_control_data.set_index("string_date")
+
+        #tmp_filename = "./tmp/" + datetime.datetime.now().isoformat().replace(":","_") + ".txt"
 
         # Save file to disk
-        treatment_control_data.to_csv(tmp_filename ,sep=",",index_label="string_date",na_rep="0")
+        #treatment_control_data.to_csv(tmp_filename ,sep=",",index_label="string_date",na_rep="0")
 
         # Finally run R Causal Impact
         
-        impact, casual_impact_data = self.runR_CausalImpact(treatment_control_data,tmp_filename,
-            datetime.datetime.fromtimestamp(args["data"]["pre_start"]).isoformat(),
-            datetime.datetime.fromtimestamp(args["data"]["pre_end"]).isoformat(),
-            datetime.datetime.fromtimestamp(args["data"]["post_start"]).isoformat(),
-            datetime.datetime.fromtimestamp(args["data"]["post_end"]).isoformat()
-        )
+        #impact, casual_impact_data = self.runR_CausalImpact(treatment_control_data,tmp_filename,
+        #    datetime.datetime.fromtimestamp(args["data"]["pre_start"]).isoformat(),
+        #    datetime.datetime.fromtimestamp(args["data"]["pre_end"]).isoformat(),
+        #    datetime.datetime.fromtimestamp(args["data"]["post_start"]).isoformat(),
+        #    datetime.datetime.fromtimestamp(args["data"]["post_end"]).isoformat()
+        #)
         
         # Remove tmp file.  Comment out if debugging
         #os.remove(tmp_filename)
 
         # Turn into json
-        ts_results = casual_impact_data.to_json(orient='records')
+        ts_results = final.to_json(orient='records')
         ret_results = {"ts_results":json.loads(ts_results),"total_impact":impact}
         return ret_results
 
